fe8645e1b53b0fced110fdfea7eee7185eaeb1b5
ScottChiuNYC <scott.chiu@rutgers.edu>
2025-10-02 20:29:59 -0400

    auto save

--- fixedincome2025/__init__.py
+++ fixedincome2025/__init__.py
@@ -2,12 +2,19 @@ import pandas as pd
 from IPython.display import HTML
 
 def table(filename):
-    df = pd.read_csv(f'tables/{filename}.csv')
-    
-    # Convert to HTML and wrap in a div with centered styling
-    html = f"""
-    <div style="display: flex; justify-content: center;">
-        {df.to_html(index=False)}
-    </div>
-    """
-    return HTML(html)
+    df = pd.read_csv(f'tables/{filename}.csv', index_col=0)
+
+    return df
+
+# def table(filename):    
+#     ### Dilemma: If convert to HTML and wrap in a div with centered styling, 
+#     ### the table is centered on slides, but will not show in pdf. 
+
+#     df = pd.read_csv(f'tables/{filename}.csv')
+#     html = f"""
+#     <div style="display: flex; justify-content: center;">
+#         {df.to_html(index=False)}
+#     </div>
+#     """
+#     return HTML(html)
+
