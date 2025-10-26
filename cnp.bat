08046f43e213e8e9bf08ba1aeeb972a5d6a9134a
ScottChiuNYC <scott.chiu@rutgers.edu>
2025-10-26 00:06:02 -0400

    auto save

--- cnp.bat
+++ cnp.bat
@@ -1,4 +1,4 @@
-jupyter nbconvert --to slides --no-input docs/source/09*.ipynb --output-dir=docs/slides/
+jupyter nbconvert --to slides --no-input docs/source/*.ipynb --output-dir=docs/slides/
 git config --global user.name "ScottChiuNYC"
 git config --global user.email "scott.chiu@rutgers.edu"
 git add *
